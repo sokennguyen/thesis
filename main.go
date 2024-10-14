186f62e3f7db12edef87e8406a52c3cd2b4dd452
sokennguyen <nsongkien@gmail.com>
2024-10-14 19:10:26 +0300

    basic flow complete

--- main.go
+++ main.go
@@ -19,19 +19,33 @@ func render(c *gin.Context, template templ.Component, status int) error {
 }
 
 
-func getHtml(c *gin.Context) {
+func getFirst(c *gin.Context) {
     c.HTML(http.StatusOK, "index.html", gin.H{})
 }
+
+func getFlow(c *gin.Context) {
+    c.HTML(http.StatusOK, "start.html", gin.H{})
+}
+
 
 func main() {
 
     r := gin.Default()
 
+    //load statics
     r.Static("/assets", "./static/assets")
+    r.Static("/flow", "./static/flow")
+    r.Static("/survey", "./static/survey")
     r.StaticFile("/normalize.css", "./static/normalize.css")
     r.StaticFile("/style.css", "./static/style.css")
-    r.LoadHTMLFiles("static/index.html")
-	r.GET("/", getHtml)
+    r.StaticFile("/flow.css", "./static/flow.css")
+    //Gin can only load one of this function
+    r.LoadHTMLFiles("static/index.html", "static/flow/start.html")
+
+
+	r.GET("/", getFlow)
+	r.GET("/first-ver", getFirst)
+
 
 	r.Run(":8080")
 }
