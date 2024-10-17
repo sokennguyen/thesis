01f4a4608923b0f5208252fe1dad66c49ac26b4b
sokennguyen <nsongkien@gmail.com>
2024-10-17 15:39:14 +0300

    init db

---
+++ static/db/db_init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS main (
+    pid INT PRIMARY KEY,
+    age INT,
+    conversion BOOL,
+    bounce BOOL,
+    top INT,
+    hero_time INT,
+    feat_list_time INT,
+    ben_list_time INT,
+    feat1_time INT,
+    feat2_time INT,
+    feat3_time INT,
+    feat4_time INT,
+    screen_width VARCHAR(255),
+    screen_height VARCHAR(255),
+    country VARCHAR(255)
+)
