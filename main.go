89c46c0f163d57a3257ada6bf9857cfb2c31b656
sokennguyen <nsongkien@gmail.com>
2024-10-18 18:50:43 +0300

    save data to db

--- main.go
+++ main.go
@@ -2,24 +2,40 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 
 
     "fmt"
     "io"
+    "strconv" 
 
 	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 
+    "encoding/json"
     "database/sql"
     _ "github.com/mattn/go-sqlite3"
 
 
 )
 
+var count int = 0;
+
 type Test struct {
 	Age  int
 	ScreenWidth int
 	ScreenHeight  int
+}
+
+type AgeScreenData struct {
+    Age int
+	ScreenWidth int
+	ScreenHeight  int
+}
+
+type InteractionData struct {
+    Hovers map[string]float64 `json:"hovers"`
+    Clicks map[string]float64 `json:"clicks"`
 }
 
 
@@ -31,19 +47,166 @@ func getFlow(c *gin.Context) {
     c.HTML(http.StatusOK, "start.html", gin.H{})
 }
 
+func postFirst(c *gin.Context) {
+    body, _ := io.ReadAll(c.Request.Body)
+
+    //decode the JSON body
+    var interaction InteractionData
+    err := json.Unmarshal(body, &interaction )
+    if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Error parsing JSON"})
+		return
+    }
+
+    //combine hovers and clicks into a single map for SQL update
+    combinedData := make(map[string]float64)
+	for key, value := range interaction.Clicks {
+		combinedData[key] = value
+	}
+    for key, value := range interaction.Hovers {
+		combinedData[key] = value
+	}
+    fmt.Println(combinedData)
+
+    db, err := sql.Open("sqlite3", "./static/db/thesis.db")
+    checkErr(err)
+    stmt, err := db.Prepare(`
+            UPDATE main 
+            SET click_nav_feat = ?,
+                click_nav_price = ?,
+                click_nav_login = ?,
+                click_nav_start = ?,
+                click_hero_cta = ?,
+                click_hero_login = ?,
+                click_small_feat1_pic = ?,
+                click_small_feat2_pic = ?,
+                click_small_feat3_pic = ?,
+                click_headstart = ?,
+                click_consistency = ?,
+                click_determination = ?,
+                click_big_feat1_img = ?,
+                click_big_feat2_img = ?,
+                click_big_feat3_img = ?,
+                click_big_feat4_img = ?,
+                click_ending_cta_btn = ?,
+                
+                top = ?,
+                hover_hero = ?,
+                hover_feat_list = ?,
+                hover_benefit_list = ?,
+                hover_big_feat_1 = ?,
+                hover_big_feat_2 = ?,
+                hover_big_feat_3 = ?,
+                hover_big_feat_4 = ?,
+                hover_head_logo = ?,
+                hover_hero_title = ?,
+                hover_sub_title = ?,
+                hover_headstart_desc = ?,
+                hover_consistency_desc = ?,
+                hover_flexible_desc = ?,
+                hover_determination_desc = ?,
+                hover_big_feat1_desc = ?,
+                hover_big_feat2_desc = ?,
+                hover_big_feat3_desc = ?,
+                hover_big_feat4_desc = ?,
+                hover_ending_title = ?,
+                hover_ending_subtitle = ?,
+                hover_ending_cta_btn = ?,
+                hover_footer_logo = ?,
+                hover_footer_product = ?,
+                hover_footer_company = ?,
+                hover_footer_legal = ? 
+            WHERE rowid = ?
+    `)
+    checkErr(err)
+
+    rowid := c.Query("id")
+    fmt.Println("rowid: " + rowid)
+
+    if rowid == "" {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Missing rowid parameter"})
+        return
+    }
+
+
+    data := []interface{}{
+        combinedData["nav-feat"],
+        combinedData["nav-price"],
+        combinedData["nav-login"],
+        combinedData["nav-start"],
+        combinedData["hero-cta"],
+        combinedData["hero-login"],
+        combinedData["small-feat1-pic"],
+        combinedData["small-feat2-pic"],
+        combinedData["small-feat3-pic"],
+        combinedData["headstart"],
+        combinedData["consistency"],
+        combinedData["determination"],
+        combinedData["big-feat1-img"],
+        combinedData["big-feat2-img"],
+        combinedData["big-feat3-img"],
+        combinedData["big-feat4-img"],
+        combinedData["ending-cta-btn"],
+
+        combinedData["top"],
+        combinedData["hero"],
+        combinedData["feat-list"],
+        combinedData["benefit-list"],
+        combinedData["big-feat-1"],
+        combinedData["big-feat-2"],
+        combinedData["big-feat-3"],
+        combinedData["big-feat-4"],
+        combinedData["head-logo"],
+        combinedData["hero-title"],
+        combinedData["sub-title"],
+        combinedData["headstart-desc"],
+        combinedData["consistency-desc"],
+        combinedData["flexible-desc"],
+        combinedData["determination-desc"],
+        combinedData["big-feat1-desc"],
+        combinedData["big-feat2-desc"],
+        combinedData["big-feat3-desc"],
+        combinedData["big-feat4-desc"],
+        combinedData["ending-title"],
+        combinedData["ending-subtitle"],
+        combinedData["ending-cta-btn"],
+        combinedData["footer-logo"],
+        combinedData["footer-product"],
+        combinedData["footer-company"],
+        combinedData["footer-legal"],
+
+        rowid, // The user ID to identify the row to update
+    }
+
+    _,err = stmt.Exec(data...)
+    checkErr(err)
+}
+
 func agePost(c *gin.Context) {
     //TODO: gen new ID here
     //TODO: get age from response here
     body, _ := io.ReadAll(c.Request.Body)
-    fmt.Println(string(body))
+    fmt.Println(string(body)) //this value is byte
+    stringedBody := string(body)
 
-    //connect db
+    //parse stringedBody
+    values, err := url.ParseQuery(stringedBody) 
+    checkErr(err)
+
+    // Accessing the values
+    age := values.Get("age")
+    swidth := values.Get("swidth")
+    sheight := values.Get("sheight")
+
+
+    //connect and insert 
     db, err := sql.Open("sqlite3", "./static/db/thesis.db")
     checkErr(err)
-    stmt, err := db.Prepare("INSERT INTO main(age) values(?)")
+    stmt, err := db.Prepare("INSERT INTO main(age, screen_width, screen_height) values(?,?,?)")
     checkErr(err)
-    res, err := stmt.Exec(12313132);
+    res, err := stmt.Exec( age, swidth, sheight);
     checkErr(err)
+
 
     //check insert
     id, err := res.LastInsertId()
@@ -59,9 +222,10 @@ func agePost(c *gin.Context) {
         fmt.Printf("Age is %s", age)
     }
 
-
-    c.Redirect(http.StatusFound, "/flow/explain.html?id=321")
+    count = count+1;
+    c.Redirect(http.StatusFound, "/flow/explain.html?id="+strconv.Itoa(count))
 }
+
 
 func checkErr(err error) {
     if err != nil {
@@ -70,10 +234,6 @@ func checkErr(err error) {
 }
 
 
-func postFirst(c *gin.Context) {
-    body, _ := io.ReadAll(c.Request.Body)
-    fmt.Println(string(body))
-}
 
 
 func render(c *gin.Context, template templ.Component, status int) error {
@@ -105,3 +265,4 @@ func main() {
 
 	r.Run(":8080")
 }
+
--- static/script.js
+++ static/script.js
@@ -1,45 +1,124 @@
+const clickElements = [
+    'nav-feat', 'nav-price', 'nav-login', 'nav-start', 
+    'hero-cta', 'hero-login', 'small-feat1-pic', 
+    'small-feat2-pic', 'small-feat3-pic', 'headstart', 
+    'consistency','headstart', 'determination', 'big-feat1-img', 
+    'big-feat2-img', 'big-feat3-img', 'big-feat4-img', 
+    'ending-cta-btn'
+];
+
+const hoverTracks = [
+
+    ...clickElements,
+    
+    //sections
+    'hero',
+    'feat-list',
+    'benefit-list',
+    'big-feat-1',
+    'big-feat-2',
+    'big-feat-3',
+    'big-feat-4',
+
+    //other elements
+    'head-logo',
+    'hero-title',
+    'sub-title',
+    'headstart-desc',
+    'consistency-desc',
+    'flexible-desc',
+    'determination-desc',
+    'big-feat1-desc',
+    'big-feat2-desc',
+    'big-feat3-desc',
+    'big-feat4-desc',
+    'ending-title',
+    'ending-subtitle',
+    'ending-cta-btn',
+    'footer-logo',
+    'footer-product',
+    'footer-company',
+    'footer-legal',
+];
+
+let clickCountsObj = {};
+
 //TimeMe is a time measuring library included in html
 window.onload = function() {
+    //track hover time
     TimeMe.initialize({
         currentPageName: "my-home-page", // current page
-        idleTimeoutInSeconds: 5, 
+        idleTimeoutInSeconds: 10, 
     });  
-    TimeMe.trackTimeOnElement('hero');
-    TimeMe.trackTimeOnElement('feat-list');
-    TimeMe.trackTimeOnElement('benefit-list');
-    TimeMe.trackTimeOnElement('big-feat-1');
-    TimeMe.trackTimeOnElement('big-feat-2');
-    TimeMe.trackTimeOnElement('big-feat-3');
-    TimeMe.trackTimeOnElement('big-feat-4');
+
+    /* 
+    let clickCounts = {
+        'nav-feat': 0,
+        'nav-price': 0,
+        'nav-login': 0,
+        'nav-start': 0,
+        'hero-cta': 0,
+        'hero-login': 0,
+        'small-feat1-img': 0,
+        'small-feat2-img': 0,
+        'small-feat3-img': 0,
+        'headstart': 0,
+        'consistency': 0,
+        'determination': 0,
+        'big-feat1-img': 0,
+        'big-feat2-img': 0,
+        'big-feat3-img': 0,
+        'big-feat4-img': 0,
+        'ending-cta-btn': 0,
+    } 
+    */
+
+
+
+
+    // Initialize clickCounts object
+    clickElements.forEach(id => {
+        clickCountsObj[id] = 0;
+    });
+
+    const incrementOnclick = (e) => {
+        const id = e.target.id 
+        clickCountsObj[id] += 1;
+        console.log(clickCountsObj)
+    }
+
+    clickElements.forEach(id => {
+        const element = document.getElementById(id);
+        if (element) {
+            element.addEventListener("click", incrementOnclick);
+        }
+    });
+
+
+    hoverTracks.forEach(id => {
+        //TimeMe initialized aboved
+        TimeMe.trackTimeOnElement(id);
+    })
 }
 
-function getIdFromParam() {
-    
-};
 
 function sendPostRequest(event) {
     xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("POST","/first-ver",true);
+    const id = window.location.search.substr(1)
+    xmlhttp.open("POST","/first-ver?"+id,true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
     let timeSpentOnPage = TimeMe.getTimeOnCurrentPageInSeconds();
-    let heroTime = TimeMe.getTimeOnElementInSeconds('hero');
-    let featListTime = TimeMe.getTimeOnElementInSeconds('feat-list');
-    let benefitTime = TimeMe.getTimeOnElementInSeconds('benefit-list');
-    let feat1Time = TimeMe.getTimeOnElementInSeconds('big-feat-1');
-    let feat2Time = TimeMe.getTimeOnElementInSeconds('big-feat-2');
-    let feat3Time = TimeMe.getTimeOnElementInSeconds('big-feat-3');
-    let feat4Time = TimeMe.getTimeOnElementInSeconds('big-feat-4');
-    let returnObj = {
-        "top": timeSpentOnPage,
-        "hero": heroTime,
-        "featList": featListTime,
-        "benList": benefitTime,
-        "feat1": feat1Time,
-        "feat2": feat2Time,
-        "feat3": feat3Time,
-        "feat4": feat4Time,
+    let hoverObj = {"top": timeSpentOnPage}
+    hoverTracks.forEach(id => {
+        hoverObj[id] = TimeMe.getTimeOnElementInSeconds(id)
+    })
+
+    let testObj = {
+        "hovers": hoverObj,
+        "clicks": clickCountsObj
     }
-    let stringReturn = JSON.stringify(returnObj)
+    let stringReturn = JSON.stringify(testObj)
     xmlhttp.send(stringReturn);
 };
 
